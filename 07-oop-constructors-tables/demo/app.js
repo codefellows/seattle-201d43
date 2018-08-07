2224a54ad5979195f294dbe65d858f74fcd65bb6
mnfmnfm <mnfmnfm@gmail.com>
2018-08-07 11:59:48 -0700

    demo day 7

--- 07-oop-constructors-tables/demo/app.js
+++ 07-oop-constructors-tables/demo/app.js
@@ -1,44 +1,76 @@
 console.log('js loaded');
 
+// global variables
+var myName = 'Michelle';
+
+
+// set up instance methods that I will attach to the constructor
+
+// add one person's info to the page
+function addInfo() {
+  var tbody = document.getElementById('main-content');
+  var personInfo = document.createElement('tr');
+  var personName = document.createElement('td');
+  var personShoeSize = document.createElement('td');
+  var personEyeColor = document.createElement('td');
+  var personShirtColor = document.createElement('td');
+  personName.className = 'blue';
+
+  personName.textContent = this.name;
+  personShoeSize.textContent = this.shoeSize;
+  personEyeColor.textContent = this.eyeColor;
+  personShirtColor.textContent = this.shirtColor;
+  personInfo.appendChild(personName);
+  personInfo.appendChild(personShoeSize);
+  personInfo.appendChild(personEyeColor);
+  personInfo.appendChild(personShirtColor);
+  tbody.appendChild(personInfo);
+}
+
+// one person starts the class
 function startTheClass() {
   console.log('Hello! My name is ' + this.name + '. Let\'s talk about ' + this.classTopic + '.');
 }
 
-var myName = 'Michelle';
+// set up a constructor
+function Person(name, shoeSize, eyeColor, shirtColor) {
+  this.name = name;
+  this.shoeSize = shoeSize;
+  this.eyeColor = eyeColor;
+  this.shirtColor = shirtColor;
+  this.addInfo = addInfo; // BAD DON'T DO THIS
+}
+Person.prototype.addInfo = addInfo;
+Person.prototype.startClass = startTheClass;
 
-var justin = {
-  name: 'Justin',
-  shoeSize: 11.5,
-  eyeColor: 'green',
-  shirtColor: 'gray',
-  startClass: startTheClass,
-  classTopic: 'puppies',
-  addInfo: addPersonInfo
-};
 
-var me = {
-  name: myName,
-  shoeSize: 10,
-  eyeColor: 'blue',
-  shirtColor: 'black',
-  startClass: startTheClass,
-  classTopic: 'Objects',
-  tas: [justin,
-    {
-      name: 'joana',
-      shoeSize: 7.5
-    },
-    {
-      name: 'suzanne',
-      shoeSize: 7
-    }],
-  addInfo: addPersonInfo
-};
 
+// runner code: actually do stuff
+var me = new Person('Michelle', 10, 'blue', 'gray');
+var justin = new Person('Justin', 11.5, 'green', 'gray');
 var heading = document.getElementById('header');
 var h2 = document.createElement('h2');
 h2.textContent = 'we made this h2';
 heading.appendChild(h2);
+
+me.addInfo();
+justin.addInfo();
+
+
+
+// zombie code from yesterday
+
+
+
+// var justin = {
+//   name: 'Justin',
+//   shoeSize: 11.5,
+//   eyeColor: 'green',
+//   shirtColor: 'gray',
+//   startClass: startTheClass,
+//   addInfo: addPersonInfo
+// };
+
 
 
 // add one person's info to the page
@@ -72,26 +104,3 @@ heading.appendChild(h2);
 //   personInfo.appendChild(personShoeSizeDiv);
 //   ul.appendChild(personInfo);
 // }
-
-// add one person's info to the page
-function addPersonInfo() {
-  var tbody = document.getElementById('main-content');
-  var personInfo = document.createElement('tr');
-  var personName = document.createElement('td');
-  var personShoeSize = document.createElement('td');
-  var personEyeColor = document.createElement('td');
-  var personShirtColor = document.createElement('td');
-
-  personName.textContent = this.name;
-  personShoeSize.textContent = this.shoeSize;
-  personEyeColor.textContent = this.eyeColor;
-  personShirtColor.textContent = this.shirtColor;
-  personInfo.appendChild(personName);
-  personInfo.appendChild(personShoeSize);
-  personInfo.appendChild(personEyeColor);
-  personInfo.appendChild(personShirtColor);
-  tbody.appendChild(personInfo);
-}
-
-me.addInfo();
-justin.addInfo();
