b4c90dedb4efa8ccf7abbd5821e008a0610d944d
mnfmnfm <mnfmnfm@gmail.com>
2018-08-13 11:32:55 -0700

    goat constructor added

--- 11-av-practical-clicktracker/demo/app.js
+++ 11-av-practical-clicktracker/demo/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// constructor for goats
+function Goat(filename) {
+  this.filename = filename;
+  this.votes = 0;
+  Goat.allGoats.push(this);
+}
+Goat.allGoats = [];
+
+new Goat('img/cruisin-goat.jpg');
+new Goat('img/kissing-goat.jpg');
+new Goat('img/sassy-goat.jpg');
+new Goat('img/smiling-goat.jpg');
+new Goat('img/sweater-goat.jpg');
