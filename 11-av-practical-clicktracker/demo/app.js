df50d911ec56fdb95645bf8e074669698502134a
mnfmnfm <mnfmnfm@gmail.com>
2018-08-13 11:57:16 -0700

    no voting for the same goat twice on one page

--- 11-av-practical-clicktracker/demo/app.js
+++ 11-av-practical-clicktracker/demo/app.js
@@ -14,6 +14,19 @@ var goat1 = new Goat('img/sassy-goat.jpg');
 var goat2 = new Goat('img/smiling-goat.jpg');
 new Goat('img/sweater-goat.jpg');
 
+function displayTwoNewGoats() {
+  // show new pictures to user
+  // grab 2 goats at random
+  var randIndex = Math.floor(Math.random() * Goat.allGoats.length);
+  goat1 = Goat.allGoats[randIndex];
+  do {
+    var secondGoatIndex = Math.floor(Math.random() * Goat.allGoats.length);
+  } while (randIndex === secondGoatIndex);
+  goat2 = Goat.allGoats[secondGoatIndex];
+  // change img src on the page to match the 2 new goats
+  img1.src = goat1.filename;
+  img2.src = goat2.filename;
+}
 
 // event listeners
 // where are we listening? the images
@@ -21,19 +34,16 @@ var img1 = document.getElementsByTagName('img')[0];
 var img2 = document.getElementsByTagName('img')[1];
 // what are we listening for? click
 img1.addEventListener('click', function() {
-// what should we do?
   // add to votes for that goat
   goat1.votes++;
   // goat1.votes = goat1.votes + 1;
-
-  // show new pictures to user
-  // grab 2 goats at random
-  var randIndex = Math.floor(Math.random() * Goat.allGoats.length);
-  goat1 = Goat.allGoats[randIndex];
-  randIndex = Math.floor(Math.random() * Goat.allGoats.length);
-  goat2 = Goat.allGoats[randIndex];
-  // change img src on the page to match the 2 new goats
-  img1.src = goat1.filename;
-  img2.src = goat2.filename;
+  displayTwoNewGoats();
 });
 
+img2.addEventListener('click', function() {
+  // what should we do?
+  // add to votes for that goat
+  goat2.votes++;
+  // goat1.votes = goat1.votes + 1;
+  displayTwoNewGoats();
+});
